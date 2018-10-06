ef54e7ce17a7ce18bb082f4acf045d9ee3994d9a
league <league@wts13.attlocal.net>
2018-10-06 12:55:53 -0700

    String Methods #1

--- src/charAt_scanners_optionDialogs/StringMethods1.java
+++ src/charAt_scanners_optionDialogs/StringMethods1.java
@@ -7,24 +7,37 @@ package charAt_scanners_optionDialogs;
 public class StringMethods1 {
 	public static void main(String[] args) {
 		
+				
 		// 1. Create a String variable and initialize it to whatever you want
-
+		String Variable="Variable's Message";
+		
+		 System.out.println(Variable);
 		// 2. Print the 3rd char of your String to the console.
-		//    HINT: .charAt
-		
+//		    HINT: .charAt
+            System.out.println(Variable.charAt(0));
 		// 3. Print the length of your String to the console.
-		//    HINT: .length()
-
+//		    HINT: .length()
+           System.out.println(Variable.length());
 		// 4. Using a for loop, print one char at time to the console.
-		//    HINT: use .length() to determine how many loops
-
-		
+//		    HINT: use .length() to determine how many loops
+            for(int i=0;i<Variable.length();i++) {
+            	 System.out.println("i="+i+" "+Variable.charAt(i));
+            }
 		// 5. Pick a char inside your String, and use a loop to determine
-		//    what position/index in the String the char is located.
-		//    Print the char's position to the console.
-		//    EXAMPLE: if your string is "abc" and you are searching 
-		//             for char 'b', then print "b is at index 1"
+//		    what position/index in the String the char is located.
+//		    Print the char's position to the console.
+//		    EXAMPLE: if your string is "abc" and you are searching 
+//		             for char 'b', then print "b is at index 1"
 		
+		   char ch='l';
+		   
+		   for(int i=0;i<Variable.length();i++) {
+			   if(Variable.charAt(i)==ch) {
+				   System.out.println(ch+" is at index " + i);
+			   }
+          }
+		
+            
 	}
 }
 
