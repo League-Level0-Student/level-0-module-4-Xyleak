7285b936b2afb44f1639f24a2fddb39fae619c2a
elliewetzel <league@worthymini-5.attlocal.net>
2018-10-27 12:54:27 -0700

    A Near Done Happy Pet Program.

--- src/charAt_scanners_optionDialogs/HappyPet.java
+++ src/charAt_scanners_optionDialogs/HappyPet.java
@@ -13,22 +13,21 @@ public class HappyPet {
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
 		
-String pet = JOptionPane.showInputDialog("wAt p3T d0 U w4nt.!?");
+//String pet = JOptionPane.showInputDialog("What Pet Do You Want");
 
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
 		
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-for (int i = 3; i < args.length; i++) {
-	
+for (int i = 3; i < 9; i++) {
+	String pet = JOptionPane.showInputDialog("What Pet Do You Want");
 
 
-			int task = JOptionPane.showOptionDialog(null, "WuT U d0", "#O#O#F#", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "00f", "pL4Y", "Ctrl QQ", "Bleach" }, null);
-            
-			
-			// 5. Use user input to call the appropriate method created in step 4.
+			int task = JOptionPane.showOptionDialog(null, "What Do You Do?", "Deku", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Green", "Yellow", "Red", "Pink" }, null);
+            task  = (1);
+			//if(1 = "Green") {JOptionPane.showMessageDialog(null,"Your Pet Is Now Very Emotional"); break;}
 
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
@@ -39,6 +38,5 @@ for (int i = 3; i < args.length; i++) {
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
 }
-	}
-	
-	}
+}
+}
